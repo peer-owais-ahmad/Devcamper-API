3c2255e1309049b5e57ce69133c9466b36f5fd2b
peer-owais-ahmad <saeedowaiss@gmail.com>
2022-11-18 23:51:58 +0530

    my first commit

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,24 +1,22 @@
 const express = require('express');
+const { getBootcamps,
+        getBootcamp,
+        createBootcamp,
+        updateBootcamp,
+        deleteBootcamp} = require('../controllers/bootcamp');
+
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.status(200).json({success: true, msg: 'show all bootcamps'});
-});
+router.
+route('/')
+.get(getBootcamps)
+.post(createBootcamp);
 
-router.get('/:id', (req,res) => {
-    res.status(200).json({success: true, msg: `show bootcamp ${req.params.id}`});
-}) 
+router.
+route('/:id')
+.get(getBootcamp)
+.put(updateBootcamp)
+.delete(deleteBootcamp);
 
-router.post('/', (req,res) => {
-    res.status(200).json({success: true, msg: 'create new bootcamp'});
-});
-
-router.put('/:id', (req,res) => {
-    res.status(200).json({success: true, msg: `update bootcamp ${req.params.id}`});
-});
-
-router.delete('/:id', (req,res) => {
-    res.status(200).json({success: true, msg: `delete bootcamp ${req.params.id}`});
-});
 
 module.exports = router;
