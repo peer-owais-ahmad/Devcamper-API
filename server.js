4acefc7d850b725d981a32eefea08e6427a01fff
peer-owais-ahmad <saeedowaiss@gmail.com>
2022-12-03 19:08:38 +0530

    Added readme and starter config

--- server.js
+++ server.js
@@ -89,6 +89,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
+
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 //Handle unhandled  promise rejections
